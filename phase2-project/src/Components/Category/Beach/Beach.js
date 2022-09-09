4aa0fa0337c0aa0d06639df1551baf5cbdeb7e69
JustinMitchell <mitchelljm04@gmail.com>
2022-09-08 21:50:26 -0400

    Category Beach Updated

---
+++ phase2-project/src/Components/Category/Beach/Beach.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Beach.css'
+
+const Beach = () => {
+  return (
+    <div className='mainDiv'>
+        <div className='beachTitle'>
+            <span>Beaches</span>
+        </div>
+        <div className='post'>
+            <div className='postmain'>
+                <img src="/Images/beaches/figurepools.jpg"></img>
+                <div className='postInfo'>
+                    <h1>Figure 8 Pools Trip</h1>
+                    <p>Just outside of Sydney is Royal National Park which has tons of tourist destinations to visit. On this trip we were on the hunt for the figure 8 pools which can only be seen during specific times due to the tides. After about an hour and a half hike from the car park through some dense bush with probably many snakes. You come out onto Burning Palms beach which is shown in the picture above. One of the beach beaches I went to while living in Australia due to its remoteness and beautiful landscape. The figure 8 pools were just around the corner, and they were a little under whelming due to the hype of these pools. Although it was a great trip to go on. 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Beach
